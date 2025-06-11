b165289260ca390b4a3012f07d0516ff36f1c7c5
waffle7113 <51526690+waffle7113@users.noreply.github.com>
2025-06-11 14:12:22 +0900

    44

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     // Google Sheets API 설정
-    SPREADSHEET_ID: '1eKqgDEXw9Ib9ukZRSi6LS_nLqr2d8KOThRDM6WiGArQ', // 여기에 실제 스프레드시트 ID를 입력하세요
-    API_KEY: 'AIzaSyAmkb_150gyco4uaVDGD0qLXgpgc5u_Ibk', // 여기에 실제 API 키를 입력하세요
+    SPREADSHEET_ID: 'YOUR_SPREADSHEET_ID', // 1eKqgDEXw9Ib9ukZRSi6LS_nLqr2d8KOThRDM6WiGArQ
+    API_KEY: 'YOUR_API_KEY', // AIzaSyAmkb_150gyco4uaVDGD0qLXgpgc5u_Ibk
     SHEET_NAMES: {
         STUDENTS: 'Students',
         ASSIGNMENTS: 'Assignments',
